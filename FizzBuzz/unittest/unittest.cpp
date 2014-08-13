5870e9d6ed2b7dcd8c76be318135d5b529000d58
oreshio <oreshio+git@gmail.com>
2014-08-13 11:54:50 +0900

    UnitTestベース作成 機能を作る前段階 実装し始めるときはここから取得すると便利

---
+++ FizzBuzz/unittest/unittest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+TEST(testcase, testname)
+{
+	std::cout << "testcase" << std::endl;
+}
+
+//// Run all the tests that were declared with TEST()
+//int main(int argc, char **argv){
+//    testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
