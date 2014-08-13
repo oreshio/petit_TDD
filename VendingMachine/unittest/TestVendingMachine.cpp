ce0400091dadbf7a2a66d145f09d997f1f847961
oreshio <oreshio+git@gmail.com>
2014-08-13 19:05:18 +0900

    VendingMachine Step1 投入と総額取得のテストコード（Red）

--- VendingMachine/unittest/TestVendingMachine.cpp
+++ VendingMachine/unittest/TestVendingMachine.cpp
@@ -7,3 +7,17 @@ TEST(VendingMachine, Test_Constructor)
 	VendingMachine testee;
 }
 
+TEST(VendingMachine_Step0, putMoney)
+{
+	VendingMachine testee;
+	testee.putMoney(COIN_10);
+	EXPECT_EQ("10", testee.getTotal());
+	testee.putMoney(COIN_50);
+	EXPECT_EQ("60", testee.getTotal());
+	testee.putMoney(COIN_100);
+	EXPECT_EQ("160", testee.getTotal());
+	testee.putMoney(COIN_500);
+	EXPECT_EQ("660", testee.getTotal());
+	testee.putMoney(BILL_1000);
+	EXPECT_EQ("1660", testee.getTotal());
+}
