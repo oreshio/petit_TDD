547a4d61c5207006156c404541735702a912ee5d
oreshio <oreshio+git@gmail.com>
2014-08-13 18:14:51 +0900

    コード書いた テスト追加した あとはどんなテストを追加したらいいか？

--- MyYear/MyYear.cpp
+++ MyYear/MyYear.cpp
@@ -1,6 +1,12 @@
 #include "MyYear.h"
 #include <sstream>
 
+namespace
+{
+	const int VALUE_400 = 400;
+	const int VALUE_100 = 100;
+	const int VALUE_4 = 4;
+}
 
 MyYear::MyYear(const int year)
 	: year_(year)
@@ -20,5 +26,17 @@ std::string MyYear::toString() const
 
 bool MyYear::isLeap() const
 {
+	if (0 == year_ % VALUE_400)
+	{
+		return true;
+	}
+	else if (0 == year_ % VALUE_100)
+	{
+		return false;
+	}
+	else if (0 == year_ % VALUE_4)
+	{
+		return true;
+	}
 	return false;
 }
--- MyYear/unittest/unittest.cpp
+++ MyYear/unittest/unittest.cpp
@@ -10,3 +10,45 @@ TEST(MyYear, Test_Constructor)
 	EXPECT_EQ("1978", testee.toString());
 	EXPECT_EQ(false, testee.isLeap());
 }
+
+TEST(MyYear, Test_Uruu_4)
+{
+	MyYear testee(4);
+	EXPECT_EQ("4", testee.toString());
+	EXPECT_EQ(true, testee.isLeap());
+}
+
+TEST(MyYear, Test_Uruu_Minus1)
+{
+	MyYear testee(-1);
+	EXPECT_EQ("-1", testee.toString());
+	EXPECT_EQ(false, testee.isLeap());
+}
+
+TEST(MyYear, Test_Uruu_20)
+{
+	MyYear testee(20);
+	EXPECT_EQ("20", testee.toString());
+	EXPECT_EQ(true, testee.isLeap());
+}
+
+TEST(MyYear, Test_Uruu_100)
+{
+	MyYear testee(100);
+	EXPECT_EQ("100", testee.toString());
+	EXPECT_EQ(false, testee.isLeap());
+}
+
+TEST(MyYear, Test_Uruu_400)
+{
+	MyYear testee(400);
+	EXPECT_EQ("400", testee.toString());
+	EXPECT_EQ(true, testee.isLeap());
+}
+
+TEST(MyYear, Test_Uruu_401)
+{
+	MyYear testee(401);
+	EXPECT_EQ("401", testee.toString());
+	EXPECT_EQ(false, testee.isLeap());
+}
